856df73625fd38d3d7134a98dd617ce5bd2622a4
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-09 14:22:45 -0400

    Completley redid because I used console.assert, rather than recreated console.assert. takes two args and compares

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,14 @@
-// assert equal function
-
+// Function Implementation
 const assertEqual = function(actual, expected) {
-  
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-//test code
-assertEqual("Lighthouse Labs", "BootCamp");
-assertEqual(1,1);
+// Test code
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("round","round");
+assertEqual(1, "10");
