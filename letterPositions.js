c010f6f6ee3164787d76b661f29d8f91351c2466
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-11 08:54:44 -0400

    created file and copied assertequals and eqarray into it.

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+
+//assert equals test function
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected} `);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//equalArrays test function
+
+const eqArrays = function(arr1, arr2) {
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+};
+
+// letter Positions Function
+
+const letterPositions = function(sentance) {
+
+  const results = {};
+
+  return results;
+
+}
+
