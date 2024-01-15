bce1242f6d73bc97f9da2aa335c98d97c4dd4415
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 15:03:08 -0400

    chai tests for tail.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,25 @@
 
-const assertEqual = require("../lib/assertEqual")
-const assertArraysEqual = require("../lib/assertArraysEqual");
-const tail = require("../lib/tail");
 
-const testArr = [1,2,3,4,5];
-const tailArr = tail(testArr);
-const testArr2 = [1];
-const tailArr2 = tail(testArr2);
-assertArraysEqual(tailArr2, [] )
-assertEqual(testArr[1], tailArr[0]);
-assertEqual(testArr[2], tailArr[1]);
-assertEqual(testArr[3], tailArr[2]);
-assertEqual(testArr[4], tailArr[3]);
-//// larry this checks that the array is unmuttated!!!!
-assertEqual(testArr.length, 5);
+const tail = require("../lib/tail");
+const {assert}= require("chai");
+
+describe("#tail", () => {
+  it("returns [2,3] for [1,2,3]", () => {
+    assert.deepStrictEqual(tail([1,2,3]), [2,3]);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepStrictEqual(tail([[5]]), []); 
+  });
+  it("does Not Mutate the original Array", () => {
+    let testArr = [1,2];
+    tail(testArr);
+    assert.deepStrictEqual(testArr.length, 2)
+  })
+});
+
+
+
+
+
+
+
