c2263e9e094d02fcc6048f87154621d98f83885b
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 11:47:41 -0400

    fixed tail tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,16 @@
 
 const assertEqual = require("../assertEqual")
+const assertArraysEqual = require("../assertArraysEqual");
 const tail = require("../tail");
 
 const testArr = [1,2,3,4,5];
 const tailArr = tail(testArr);
 const testArr2 = [1];
 const tailArr2 = tail(testArr2);
-assertEqual(tailArr2, "")
+assertArraysEqual(tailArr2, [] )
 assertEqual(testArr[1], tailArr[0]);
 assertEqual(testArr[2], tailArr[1]);
 assertEqual(testArr[3], tailArr[2]);
 assertEqual(testArr[4], tailArr[3]);
+//// larry this checks that the array is unmuttated!!!!
 assertEqual(testArr.length, 5);
