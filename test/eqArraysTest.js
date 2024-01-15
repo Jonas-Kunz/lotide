ec52f3eb80f49f9c07862d5087ce6e05d1fad567
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 12:02:05 -0400

    eqArrays test complete

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,5 @@
 const eqArrays = require("../eqArrays");
+const assertEqual = require("../assertEqual");
 /////// test code //////
-console.log(eqArrays([1,2,3],[1,2,3]));
-console.log(eqArrays([1,2,3],[1,3]));
+assertEqual((eqArrays([1,2,3],[1,2,3])), true);
+assertEqual((eqArrays([1,2,3],[1,3])), false);
