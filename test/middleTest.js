9c80a6db171e93de52833eaa2723639cee8c1ff4
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 12:23:37 -0400

    tests for middle

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,7 @@
 const middle = require("../middle");
+const assertArraysEqual = require("../assertArraysEqual");
 /////// test code //////
-console.log(middle([1,2,3]));
-console.log(middle([1,2,3,4]));
-console.log(middle([1,2]));
-console.log(middle([]));
+assertArraysEqual((middle([1,2,3])), [2]);
+assertArraysEqual((middle([1,2,3,4])), [2,3]);
+assertArraysEqual((middle([1,2])), []);
+assertArraysEqual((middle([])), []);
