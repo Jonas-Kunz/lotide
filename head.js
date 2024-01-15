79fb6b2751d5af753eb097457ffc37d164358a6b
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 13:04:50 -0400

    added .gitignore

--- head.js
+++
@@ -1,7 +0,0 @@
-
-
-const head = function(arr) {
-  return arr[0];
-};
-
-module.exports = head;
--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,11 @@
-const head = require("../head")
-const assertEqual = require("../assertEqual")
+const head = require("../head");
+const assert = require("chai").assert
+const assertEqual = require("../assertEqual");
+
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+});
 /////// test code //////
-assertEqual((head([1,2,3,4,5,6,7,8])), 1);
+//assertEqual((head([1,2,3,4,5,6,7,8])), 1);
