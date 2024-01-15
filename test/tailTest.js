358bda637323c1e4cb608fc8cfec862e56cd0107
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 11:32:49 -0400

    fixed tail tests even though the requirments are strange assertArrays equal is better than assert equal for array assertions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,12 @@
-const assertArraysEqual = require("../assertArraysEqual");
+
+const assertEqual = require("../assertEqual")
 const tail = require("../tail");
-/////// test code //////
-assertArraysEqual((tail([1,2,3,4,5])), [2,3,4,5]);
+
+const testArr = [1,2,3,4,5];
+const tailArr = tail(testArr);
+
+assertEqual(testArr[1], tailArr[0]);
+assertEqual(testArr[2], tailArr[1]);
+assertEqual(testArr[3], tailArr[2]);
+assertEqual(testArr[4], tailArr[3]);
+assertEqual(testArr.length, 5);
