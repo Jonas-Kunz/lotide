edccdc2ef0c7cea6120d64bef1eef9f653eddf44
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 11:41:56 -0400

    fixing tail

--- tail.js
+++ tail.js
@@ -1,7 +1,11 @@
 
 // returns all elemnets but the first:
 const tail = function(arr) {
+  if (arr.length < 2) {
+    return []
+  }
   return arr.slice(1);
 };
-
+const arr = [1]
+console.log(tail(arr));
 module.exports = tail;
--- test/tailTest.js
+++ test/tailTest.js
@@ -4,7 +4,8 @@ const tail = require("../tail");
 
 const testArr = [1,2,3,4,5];
 const tailArr = tail(testArr);
-
+const testArr2 = [1];
+assertEqual(testArr2, [])
 assertEqual(testArr[1], tailArr[0]);
 assertEqual(testArr[2], tailArr[1]);
 assertEqual(testArr[3], tailArr[2]);
