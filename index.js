c0f8ad577c8b509f8e862bc57f55b194c20ee07a
Jonas-Kunz <jonas_K_7@outlook.com>
2024-01-15 15:44:29 -0400

    refactored index.js to use shorthand exports, added countOnly, and no Larry I wont put index.js in lotide i want to keep all the things nicely packaged like all the model packages we have been presented

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const assertArraysEqual = require("./lib/assertArraysEqual");
 const assertEqual = require("./lib/assertEqual");
 const assertObjectsEqual =require("./lib/assertObjectsEqual");
 const countLetters = require("./lib/countLetters");
+const countOnly = require("./lib/countOnly");
 const eqArrays = require("./lib/eqArrays");
 const eqObjects = require("./lib/eqObjects");
 const findKeyByValue = require("./lib/findKeyByValue");
@@ -15,19 +16,20 @@ const takeUntil = require("./lib/takeUntil");
 const without = require("./lib/without");
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKeyByValue: findKeyByValue,
-  findKey: findKey,
-  head: head,
-  letterPositions: letterPositions,
-  map: map,
-  middle: middle,
-  tail: tail,
-  takeUntil: takeUntil,
-  without: without 
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKeyByValue,
+  findKey,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
